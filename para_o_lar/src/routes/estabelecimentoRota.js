a8911cffd00485559a0766153f29031bf9c14883
Elenfrancyne <elenfrancyne@gmail.com>
2022-06-04 13:42:37 -0300

    Subindo a controller

--- para_o_lar/src/routes/estabelecimentoRota.js
+++ para_o_lar/src/routes/estabelecimentoRota.js
@@ -1,5 +1,5 @@
 const express = require('express')
-const controller = require('../controllers/estabelecimentoController')
+const controller = require('../controller/estabelecimentoController')
 const router = express.Router()
 router.get('/membros', controller.findAllMembers);
 router.get("/nome", controller.findOneByName)
